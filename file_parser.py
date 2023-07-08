237fb618b49e6605de8ad7beb5b1b6d2dd72279a
DeNeBoLas <svitlana-ilchenko@ukr.net>
2023-07-08 23:41:32 +0300

    edit

--- file_parser.py
+++ file_parser.py
@@ -5,32 +5,74 @@ JPEG_IMAGES = []
 JPG_IMAGES = []
 PNG_IMAGES = []
 SVG_IMAGES = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MOV_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+TXT_DOCUMENTS = []
+PDF_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
 MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+ZIP_ARCHIVES = []
+GZ_ARCHIVES = []
+TAR_ARCHIVES = []
 MY_OTHER = []
-ARCHIVES = []
+
 
 REGISTER_EXTENSION = {
-    'JPEG': JPEG_IMAGES,
-    'JPG': JPG_IMAGES,
-    'PNG': PNG_IMAGES,
-    'SVG': SVG_IMAGES,
-    'MP3': MP3_AUDIO,
-    'ZIP': ARCHIVES
+    "JPEG": JPEG_IMAGES,
+    "JPG": JPG_IMAGES,
+    "PNG": PNG_IMAGES,
+    "SVG": SVG_IMAGES,
+    "AVI": AVI_VIDEO,
+    "MP4": MP4_VIDEO,
+    "MOV": MOV_VIDEO,
+    "MKV": MKV_VIDEO,
+    "DOC": DOC_DOCUMENTS,
+    "DOCX": DOCX_DOCUMENTS,
+    "TXT": TXT_DOCUMENTS,
+    "PFD": PDF_DOCUMENTS,
+    "XLSX": XLSX_DOCUMENTS,
+    "PPTX": PPTX_DOCUMENTS,
+    "MP3": MP3_AUDIO,
+    "OGG": OGG_AUDIO,
+    "WAV": WAV_AUDIO,
+    "AMR": AMR_AUDIO,
+    "ZIP": ZIP_ARCHIVES,
+    "GZ": GZ_ARCHIVES,
+    "TAR": TAR_ARCHIVES,
 }
 
 FOLDERS = []
 EXTENSION = set()
 UNKNOWN = set()
 
+
 def get_extension(filename: str) -> str:
-    return Path(filename).suffix[1:].upper()  # перетворюємо розширення файлу на назву папки jpg -> JPG
+    return (
+        Path(filename).suffix[1:].upper()
+    )  # перетворюємо розширення файлу на назву папки jpg -> JPG
+
 
 def scan(folder: Path) -> None:
     for item in folder.iterdir():
         # Якщо це папка то додаємо її до списку FOLDERS і переходимо до наступного елемента папки
         if item.is_dir():
             # перевіряємо, щоб папка не була тією в яку ми складаємо вже файли
-            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+            if item.name not in (
+                "archives",
+                "video",
+                "audio",
+                "documents",
+                "images",
+                "MY_OTHER",
+            ):
                 FOLDERS.append(item)
                 # скануємо вкладену папку
                 scan(item)  # рекурсія
@@ -54,16 +96,32 @@ def scan(folder: Path) -> None:
 
 if __name__ == "__main__":
     folder_to_scan = sys.argv[1]
-    print(f'Start in folder {folder_to_scan}')
+    print(f"Start in folder {folder_to_scan}")
     scan(Path(folder_to_scan))
-    print(f'Images jpeg: {JPEG_IMAGES}')
-    print(f'Images jpg: {JPG_IMAGES}')
-    print(f'Images svg: {SVG_IMAGES}')
-    print(f'Audio mp3: {MP3_AUDIO}')
-    print(f'Archives: {ARCHIVES}')
+    print(f"Images jpeg: {JPEG_IMAGES}")
+    print(f"Images jpg: {JPG_IMAGES}")
+    print(f"Images svg: {SVG_IMAGES}")
+    print(f"Images png: {PNG_IMAGES}")
+    print(f"Videos avi: {AVI_VIDEO}")
+    print(f"Videos mov: {MOV_VIDEO}")
+    print(f"Videos mkv: {MKV_VIDEO}")
+    print(f"Videos mp4: {MP4_VIDEO}")
+    print(f"Documents doc: {DOC_DOCUMENTS}")
+    print(f"Documents docx: {DOCX_DOCUMENTS}")
+    print(f"Documents txt: {TXT_DOCUMENTS}")
+    print(f"Documents pdf: {PDF_DOCUMENTS}")
+    print(f"Documents xlsx: {XLSX_DOCUMENTS}")
+    print(f"Documents pptx: {PPTX_DOCUMENTS}")
+    print(f"Audio mp3: {MP3_AUDIO}")
+    print(f"Audio ogg: {OGG_AUDIO}")
+    print(f"Audio wav: {WAV_AUDIO}")
+    print(f"Audio amr: {AMR_AUDIO}")
+    print(f"Archives zip: {ZIP_ARCHIVES}")
+    print(f"Archives gz: {GZ_ARCHIVES}")
+    print(f"Archives tar: {TAR_ARCHIVES}")
 
-    print(f'Types of files in folder: {EXTENSION}')
-    print(f'Unknown files of types: {UNKNOWN}')
-    print(f'MY_OTHER: {MY_OTHER}')
+    print(f"Types of files in folder: {EXTENSION}")
+    print(f"Unknown files of types: {UNKNOWN}")
+    print(f"MY_OTHER: {MY_OTHER}")
 
     print(FOLDERS)
